258566822e764d4e17711d8ed805159eeb096238
maddalax <jm@madev.me>
2024-09-21 12:37:22 -0500

    fix build command

--- cli/htmgo/tasks/run/build.go
+++ cli/htmgo/tasks/run/build.go
@@ -2,6 +2,7 @@ package run
 
 import (
 	"github.com/maddalax/htmgo/cli/htmgo/tasks/astgen"
+	"github.com/maddalax/htmgo/cli/htmgo/tasks/copyassets"
 	"github.com/maddalax/htmgo/cli/htmgo/tasks/css"
 	"github.com/maddalax/htmgo/cli/htmgo/tasks/process"
 )
@@ -9,18 +10,11 @@ import (
 func Build() {
 	astgen.GenAst(process.ExitOnError)
 	css.GenerateCss(process.ExitOnError)
+
+	copyassets.CopyAssets()
+
 	process.RunOrExit("rm -rf ./dist")
 	process.RunOrExit("mkdir -p ./dist")
-
-	//process.RunOrExit("mkdir -p ./dist/assets/dist")
-
-	//dirutil.CopyDir(
-	//	"./assets/dist",
-	//	"./dist/assets/dist",
-	//	func(path string, exists bool) bool {
-	//		return true
-	//	},
-	//)
 
 	process.RunOrExit("env GOOS=linux GOARCH=amd64 go build -o ./dist/app-linux-amd64 .")
 	process.RunOrExit("go build -o ./dist/app .")
