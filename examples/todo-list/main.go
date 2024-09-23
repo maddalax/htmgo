e82ed049705211dd46944d7fe01d2cf71585e446
maddalax <jm@madev.me>
2024-09-22 22:46:54 -0500

    embed assets

--- examples/todo-list/main.go
+++ examples/todo-list/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"embed"
 	"github.com/labstack/echo/v4"
 	"github.com/maddalax/htmgo/framework/h"
 	"github.com/maddalax/htmgo/framework/service"
 	_ "github.com/mattn/go-sqlite3"
+	"io/fs"
 	"todolist/__htmgo"
 	"todolist/ent"
 	"todolist/infrastructure/db"
 )
+
+//go:embed assets/dist/*
+var StaticAssets embed.FS
 
 func main() {
 	locator := service.NewLocator()
@@ -17,10 +22,18 @@ func main() {
 		return db.Provide()
 	})
 
+	sub, err := fs.Sub(StaticAssets, "assets/dist")
+
+	if err != nil {
+		panic(err)
+	}
+
 	h.Start(h.AppOpts{
 		ServiceLocator: locator,
 		LiveReload:     true,
 		Register: func(e *echo.Echo) {
+			e.StaticFS("/public", sub)
+
 			e.Static("/public", "./assets/dist")
 			__htmgo.Register(e)
 		},
