e0593fa98f6e0b5594ef85c6c2db14d4f2fc0c66
maddalax <jm@madev.me>
2024-09-22 22:41:36 -0500

    dont build atm

--- cli/htmgo/tasks/run/build.go
+++ cli/htmgo/tasks/run/build.go
@@ -6,7 +6,6 @@ import (
 	"github.com/maddalax/htmgo/cli/htmgo/tasks/copyassets"
 	"github.com/maddalax/htmgo/cli/htmgo/tasks/css"
 	"github.com/maddalax/htmgo/cli/htmgo/tasks/process"
-	"os"
 )
 
 func Build() {
@@ -20,13 +19,13 @@ func Build() {
 	process.RunOrExit("rm -rf ./dist")
 	process.RunOrExit("mkdir -p ./dist")
 
-	flags := ""
-	if os.Getenv("CGO_ENABLED") == "1" {
-		flags = `-a -ldflags '-linkmode external -extldflags "-static"' `
-	}
-
-	process.RunOrExit(fmt.Sprintf("go build -o ./dist/app %s.", flags))
-	process.RunOrExit(fmt.Sprintf("go build -o ./dist/app %s.", flags))
+	//flags := ""
+	//if os.Getenv("CGO_ENABLED") == "1" {
+	//	flags = `-a -ldflags '-linkmode external -extldflags "-static"' `
+	//}
+	//
+	//process.RunOrExit(fmt.Sprintf("go build -o ./dist %s.", flags))
+	//process.RunOrExit(fmt.Sprintf("go build -o ./dist/app %s .", flags))
 
 	fmt.Printf("Executable built at %s\n", process.GetPathRelativeToCwd("dist"))
 }
