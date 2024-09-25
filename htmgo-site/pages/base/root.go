488b415f885007ab48180aa14671703e125f0c71
maddalax <jm@madev.me>
2024-09-25 11:00:31 -0500

    few more meta tags

--- htmgo-site/pages/base/root.go
+++ htmgo-site/pages/base/root.go
@@ -9,14 +9,21 @@ import (
 var Version = uuid.NewString()[0:6]
 
 func RootPage(children ...h.Ren) *h.Element {
+	title := "htmgo"
+	description := "build simple and scalable systems with go + htmx"
+
 	return h.Html(
 		h.HxExtension(h.BaseExtensions()),
 		h.Head(
 			h.Meta("viewport", "width=device-width, initial-scale=1"),
-			h.Meta("og:title", "htmgo"),
+			h.Meta("title", title),
+			h.Meta("charset", "utf-8"),
+			h.Meta("author", "htmgo"),
+			h.Meta("description", description),
+			h.Meta("og:title", title),
 			h.Meta("og:url", "https://htmgo.dev"),
 			h.Link("canonical", "https://htmgo.dev"),
-			h.Meta("og:description", "build simple and scalable systems with go + htmx"),
+			h.Meta("og:description", description),
 			h.LinkWithVersion("/public/main.css", "stylesheet", Version),
 			h.ScriptWithVersion("/public/htmgo.js", Version),
 			h.Raw(`
