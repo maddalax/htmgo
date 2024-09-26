a2b286e9aa33739f86e20fb6437873b83d7b36ff
maddalax <jm@madev.me>
2024-09-26 14:39:34 -0500

    fix example todo

--- framework/h/qs.go
+++ framework/h/qs.go
@@ -50,7 +50,7 @@ func (q *Qs) ToString() string {
 
 func GetQueryParam(ctx *RequestContext, key string) string {
 	value, ok := ctx.URL.Query()[key]
-	if !ok {
+	if value == nil || !ok {
 		current := ctx.currentBrowserUrl
 		if current != "" {
 			u, err := url.Parse(current)
@@ -58,6 +58,9 @@ func GetQueryParam(ctx *RequestContext, key string) string {
 				return u.Query().Get(key)
 			}
 		}
+	}
+	if len(value) == 0 {
+		return ""
 	}
 	return value[0]
 }
