c3be0e92d280edfad79547315dcf56afce71c778
maddalax <jm@madev.me>
2024-10-14 10:05:14 -0500

    few tests

--- framework/config/project_test.go
+++ framework/config/project_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestDefaultProjectConfig(t *testing.T) {
+	t.Parallel()
 	cfg := DefaultProjectConfig()
 	assert.Equal(t, true, cfg.Tailwind)
 	assert.Equal(t, 4, len(cfg.WatchIgnore))
@@ -14,6 +15,7 @@ func TestDefaultProjectConfig(t *testing.T) {
 }
 
 func TestNoConfigFileUsesDefault(t *testing.T) {
+	t.Parallel()
 	cfg := FromConfigFile("testdata")
 	assert.Equal(t, true, cfg.Tailwind)
 	assert.Equal(t, 4, len(cfg.WatchIgnore))
@@ -21,9 +23,21 @@ func TestNoConfigFileUsesDefault(t *testing.T) {
 }
 
 func TestPartialConfigMerges(t *testing.T) {
+	t.Parallel()
 	os.Mkdir("testdata", 0755)
 	defer os.RemoveAll("testdata")
 	os.WriteFile("testdata/htmgo.yaml", []byte("tailwind: false"), 0644)
+	cfg := FromConfigFile("testdata")
+	assert.Equal(t, false, cfg.Tailwind)
+	assert.Equal(t, 4, len(cfg.WatchIgnore))
+	assert.Equal(t, 8, len(cfg.WatchFiles))
+}
+
+func TestShouldNotSetTailwindTrue(t *testing.T) {
+	t.Parallel()
+	os.Mkdir("testdata1", 0755)
+	defer os.RemoveAll("testdata1")
+	os.WriteFile("testdata1/htmgo.yaml", []byte("someValue: false"), 0644)
 	cfg := FromConfigFile("testdata")
 	assert.Equal(t, false, cfg.Tailwind)
 	assert.Equal(t, 4, len(cfg.WatchIgnore))
