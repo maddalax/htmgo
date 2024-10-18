a7110576d234cea16596c4c2279caa7e11d6ab32
Kevin Lewin <97046295+lewinkedrs@users.noreply.github.com>
2024-10-18 18:29:59 -0400

    Update app.go (#24)
    Reformat server listening log message so user could click from terminal or ide

--- framework/h/app.go
+++ framework/h/app.go
@@ -182,7 +182,7 @@ func (app *App) start() {
 	}
 
 	port := ":3000"
-	slog.Info(fmt.Sprintf("Server started on port %s", port))
+	slog.Info(fmt.Sprintf("Server started at localhost:%s", port))
 	err := http.ListenAndServe(port, app.Router)
 
 	if err != nil {
